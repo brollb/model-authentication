bd291ff3ff94963cb20d6a98b5013649f033fc59
seanstory <sean.j.story@vanderbilt.edu>
2013-11-27 11:32:20 -0700

    added utility function to retrieve session requests, and corresponding test

--- src/edu/vanderbilt/cs285/test/UtilityTest.java
+++ src/edu/vanderbilt/cs285/test/UtilityTest.java
@@ -2,23 +2,100 @@ package edu.vanderbilt.cs285.test;
 
 import static org.junit.Assert.*;
 
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import edu.vanderbilt.cs285.utilities.CryptoUtilities;
 
 public class UtilityTest {
 
+	private Key psk_;
+	private KeyPair phoneKeys_;
+	private KeyPair serverKeys_;
+	private Cipher symCipher_;
+	private Cipher asymCipher_;
+	private final String username_ = "TESTUSER";
+	private IvParameterSpec iv_;
+	
+	@BeforeClass
+	public static void setUpOnce(){
+		try {
+	        java.lang.reflect.Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
+	        field.setAccessible(true);
+	        field.set(null, java.lang.Boolean.FALSE);
+	    } catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	}
+	
 	@Before
 	public void setUp() throws Exception {
+		KeyGenerator kg = KeyGenerator.getInstance(CryptoUtilities.SYMMETRIC_KEY_ALGORITHM);
+		kg.init(CryptoUtilities.SYM_KEY_SIZE);
+		psk_ = kg.generateKey();
+		
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance(CryptoUtilities.ASYMMETRIC_KEY_ALGORITHM);
+	    keyGen.initialize(1024);
+	    phoneKeys_ = keyGen.generateKeyPair();
+	    serverKeys_ = keyGen.generateKeyPair();
+	    
+	    symCipher_ = Cipher.getInstance(CryptoUtilities.SYMMETRIC_KEY_ALGORITHM + "/"
+				+ CryptoUtilities.SYM_CYPHER_MODE + "/" + CryptoUtilities.SYM_PADDING_TYPE);
+	    
+	    asymCipher_ = Cipher.getInstance(CryptoUtilities.ASYMMETRIC_KEY_ALGORITHM + "/"
+				+  CryptoUtilities.ASYM_CYPHER_MODE + "/" + CryptoUtilities.ASYM_PADDING_TYPE);
+	    
+	    SecureRandom random = new SecureRandom();
+		byte[] ivBytes = new byte[CryptoUtilities.IV_LENGTH];
+		random.nextBytes(ivBytes);
+		iv_ = new IvParameterSpec(ivBytes);
+	    
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSessionRequest() throws UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException{
+		
+		byte[] sessionRequest = CryptoUtilities.sessionRequest(psk_,iv_, username_, phoneKeys_.getPrivate());
+		
+		byte[] request = new byte[CryptoUtilities.REQUEST_LENGTH_1];
+		System.arraycopy(sessionRequest, 0, request, 0, CryptoUtilities.REQUEST_LENGTH_1);
+		
+		byte[] saltedUser = new byte[CryptoUtilities.REQUEST_LENGTH_2];
+		System.arraycopy(sessionRequest, CryptoUtilities.REQUEST_LENGTH_1, saltedUser, 0, CryptoUtilities.REQUEST_LENGTH_2);
+		
+		symCipher_.init(Cipher.DECRYPT_MODE, psk_,iv_);
+		byte[] requestPlain = symCipher_.doFinal(request);
+		assertEquals(CryptoUtilities.SESSION_REQUEST, new String(requestPlain,CryptoUtilities.PLAINTEXT_ENCODING));
+		
+		asymCipher_.init(Cipher.DECRYPT_MODE, phoneKeys_.getPublic());
+		byte[] userAndSaltPlain = asymCipher_.doFinal(saltedUser);
+		byte[] userPlain = new byte[username_.getBytes().length];
+		System.arraycopy(userAndSaltPlain, 0, userPlain, 0, userPlain.length);
+		assertEquals(username_, new String(userPlain, CryptoUtilities.PLAINTEXT_ENCODING));
 	}
 
 }
--- src/edu/vanderbilt/cs285/utilities/CryptoUtilities.java
+++ src/edu/vanderbilt/cs285/utilities/CryptoUtilities.java
@@ -1,5 +1,49 @@
 package edu.vanderbilt.cs285.utilities;
 
+import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.security.*;
+
 public class CryptoUtilities {
 
+	public static final String SYMMETRIC_KEY_ALGORITHM = "AES";
+	public static final String ASYMMETRIC_KEY_ALGORITHM = "RSA";
+	public static final String SESSION_REQUEST = "This is a session request";
+	public static final String PLAINTEXT_ENCODING = "UTF8";
+	public static final String SYM_CYPHER_MODE = "CBC";
+	public static final String ASYM_CYPHER_MODE = "ECB";
+	public static final String SYM_PADDING_TYPE = "PKCS5Padding";
+	public static final String ASYM_PADDING_TYPE = "PKCS1Padding";
+	public static final int SYM_KEY_SIZE = 256;
+	public static final int REQUEST_LENGTH_1 = 32; //E( session request, PSK )
+	public static final int REQUEST_LENGTH_2 = 128; //E( username || salt, PR )
+	public static final int SALT_LENGTH = 26;
+	public static final int IV_LENGTH = 16;
+	
+	public static byte[] sessionRequest(Key key, IvParameterSpec iv, String user, PrivateKey pk) throws UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException{
+		
+		Cipher symCipher = Cipher.getInstance(SYMMETRIC_KEY_ALGORITHM+"/"+SYM_CYPHER_MODE+"/"+SYM_PADDING_TYPE);
+		symCipher.init(Cipher.ENCRYPT_MODE, key, iv);
+		byte[] requestPlain = SESSION_REQUEST.getBytes(PLAINTEXT_ENCODING);
+		byte[] requestBytes = symCipher.doFinal(requestPlain);		
+		
+		Cipher asymCipher = Cipher.getInstance(ASYMMETRIC_KEY_ALGORITHM+"/"+ASYM_CYPHER_MODE+"/"+ASYM_PADDING_TYPE);
+		asymCipher.init(Cipher.ENCRYPT_MODE, pk);
+		SecureRandom random = new SecureRandom();
+		String salt = new BigInteger(130, random).toString(32);
+		String saltedUser = user+salt;
+		byte[] saltedUserPlainBytes = saltedUser.getBytes(CryptoUtilities.PLAINTEXT_ENCODING);
+		byte[] saltedUserBytes = asymCipher.doFinal(saltedUserPlainBytes);
+		
+		byte[] result = new byte[requestBytes.length + saltedUserBytes.length];;
+		System.arraycopy(requestBytes, 0, result, 0, requestBytes.length);
+		System.arraycopy(saltedUserBytes, 0,result,requestBytes.length,saltedUserBytes.length);
+		
+		return result;
+	}
+	
+	
 }
