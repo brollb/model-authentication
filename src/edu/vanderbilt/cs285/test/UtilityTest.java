23ee1ee74dc4b5d5477bfea0f1c18f8ffdd2e069
seanstory <sean.j.story@vanderbilt.edu>
2013-11-27 15:35:41 -0700

    formatted code

--- src/edu/vanderbilt/cs285/test/UtilityTest.java
+++ src/edu/vanderbilt/cs285/test/UtilityTest.java
@@ -3,22 +3,17 @@ package edu.vanderbilt.cs285.test;
 import static org.junit.Assert.*;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyPair;
-import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 
 import org.junit.After;
@@ -37,73 +32,88 @@ public class UtilityTest {
 	private Cipher asymCipher_;
 	private final String username_ = "TESTUSER";
 	private IvParameterSpec iv_;
-	
+
 	@BeforeClass
-	public static void setUpOnce(){
+	public static void setUpOnce() {
 		CryptoUtilities.allowEncryption();
 	}
-	
+
 	@Before
 	public void setUp() throws Exception {
 		psk_ = CryptoUtilities.getSymmetricKey();
-	    phoneKeys_ = CryptoUtilities.getKeypair(false);
-	    serverKeys_ = CryptoUtilities.getKeypair(true);
-	    
-	    symCipher_ = CryptoUtilities.getSymmetricCipher();
-	    
-	    asymCipher_ = CryptoUtilities.getAsymmetricCipher();
-	    
-	    iv_ = CryptoUtilities.getNewIV();
-	    
+		phoneKeys_ = CryptoUtilities.getKeypair(false);
+		serverKeys_ = CryptoUtilities.getKeypair(true);
+
+		symCipher_ = CryptoUtilities.getSymmetricCipher();
+
+		asymCipher_ = CryptoUtilities.getAsymmetricCipher();
+
+		iv_ = CryptoUtilities.getNewIV();
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-	
+
 	@Test
 	/*
 	 * Tests that E( session request || username || HMAC(MSG, HK), SU )
 	 */
-	public void testSessionRequest() throws UnsupportedEncodingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException{
-		
-		byte[] sessionRequest = CryptoUtilities.sessionRequest(psk_, null, username_, serverKeys_.getPublic());
+	public void testSessionRequest() throws UnsupportedEncodingException,
+			IllegalBlockSizeException, BadPaddingException,
+			InvalidKeyException, NoSuchAlgorithmException,
+			NoSuchPaddingException, InvalidAlgorithmParameterException {
+
+		byte[] sessionRequest = CryptoUtilities.sessionRequest(psk_, null,
+				username_, serverKeys_.getPublic());
 		asymCipher_.init(Cipher.DECRYPT_MODE, serverKeys_.getPrivate());
 		byte[] plainBytes = asymCipher_.doFinal(sessionRequest);
-		byte[] requestBytes = new byte[CryptoUtilities.SESSION_REQUEST.getBytes().length];
+		byte[] requestBytes = new byte[CryptoUtilities.SESSION_REQUEST
+				.getBytes().length];
 		byte[] userBytes = new byte[username_.getBytes().length];
-		byte[] hmacBytes = new byte[plainBytes.length - (CryptoUtilities.SESSION_REQUEST.getBytes().length + username_.getBytes().length)];
-		
+		byte[] hmacBytes = new byte[plainBytes.length
+				- (CryptoUtilities.SESSION_REQUEST.getBytes().length + username_
+						.getBytes().length)];
+
 		System.arraycopy(plainBytes, 0, requestBytes, 0, requestBytes.length);
-		assertEquals(CryptoUtilities.SESSION_REQUEST, new String(requestBytes, CryptoUtilities.PLAINTEXT_ENCODING));
-		
-		System.arraycopy(plainBytes, requestBytes.length, userBytes, 0, userBytes.length);
-		assertEquals(username_, new String(userBytes,CryptoUtilities.PLAINTEXT_ENCODING));
-		
-		System.arraycopy(plainBytes, requestBytes.length+userBytes.length, hmacBytes, 0, hmacBytes.length);
+		assertEquals(CryptoUtilities.SESSION_REQUEST, new String(requestBytes,
+				CryptoUtilities.PLAINTEXT_ENCODING));
+
+		System.arraycopy(plainBytes, requestBytes.length, userBytes, 0,
+				userBytes.length);
+		assertEquals(username_, new String(userBytes,
+				CryptoUtilities.PLAINTEXT_ENCODING));
+
+		System.arraycopy(plainBytes, requestBytes.length + userBytes.length,
+				hmacBytes, 0, hmacBytes.length);
 		String keyString = psk_.toString();
-		assertEquals(CryptoUtilities.hmacDigest(CryptoUtilities.SESSION_REQUEST+username_, keyString), new String(hmacBytes,CryptoUtilities.PLAINTEXT_ENCODING));
-		
-		
+		assertEquals(CryptoUtilities.hmacDigest(CryptoUtilities.SESSION_REQUEST
+				+ username_, keyString), new String(hmacBytes,
+				CryptoUtilities.PLAINTEXT_ENCODING));
+
 	}
-	
+
 	@Test
 	/*
 	 * Tests E( E( SK0 || TL, PU) , SR) is returned by getNewSessionResponse()
 	 */
-	public void testGetNewSessionResponse() throws InvalidKeyException, IllegalBlockSizeException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException{
-		byte[] sessionResponse = CryptoUtilities.getNewSessionResponse(phoneKeys_.getPublic(), serverKeys_.getPrivate(), 10);
-		
+	public void testGetNewSessionResponse() throws InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException,
+			NoSuchAlgorithmException, NoSuchPaddingException {
+		byte[] sessionResponse = CryptoUtilities.getNewSessionResponse(
+				phoneKeys_.getPublic(), serverKeys_.getPrivate(), 10);
+
 		asymCipher_.init(Cipher.DECRYPT_MODE, serverKeys_.getPublic());
 		byte[] halfway = asymCipher_.doFinal(sessionResponse);
-		
+
 		asymCipher_.init(Cipher.DECRYPT_MODE, phoneKeys_.getPrivate());
 		byte[] response = asymCipher_.doFinal(halfway);
-		
-		byte[] tlBytes = new byte[4];//one int worth of bytes
-		System.arraycopy(response, response.length-4, tlBytes, 0, 4);
+
+		byte[] tlBytes = new byte[4];// one int worth of bytes
+		System.arraycopy(response, response.length - 4, tlBytes, 0, 4);
 		assertEquals(10, ByteBuffer.wrap(tlBytes).getInt());
-		
+
 	}
 
 }
--- src/edu/vanderbilt/cs285/utilities/CryptoUtilities.java
+++ src/edu/vanderbilt/cs285/utilities/CryptoUtilities.java
@@ -5,7 +5,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.security.*;
 
@@ -22,145 +21,166 @@ public class CryptoUtilities {
 	public static final String ASYM_PADDING_TYPE = "PKCS1Padding";
 	public static final int SYM_KEY_SIZE = 256;
 	public static final int ASYM_KEY_SIZE = 1024;
-	public static final int REQUEST_LENGTH_1 = 32; //E( session request, PSK )
-	public static final int REQUEST_LENGTH_2 = 128; //E( username || salt, PR )
+	public static final int REQUEST_LENGTH_1 = 32; // E( session request, PSK )
+	public static final int REQUEST_LENGTH_2 = 128; // E( username || salt, PR )
 	public static final int SALT_LENGTH = 26;
 	public static final int IV_LENGTH = 16;
-	
+
 	/*
-	 * returns the byte[] that represents E( session request || username || HMAC(MSG, HK), SU )
-	 * that should be sent from the phone to the server every time a new session key is needed.
-	 * As parameters, it takes the Symmetric Secret key, the current session key (null if the first time),
-	 * the username for the client, and the server's public key to encrypt the whole thing.
+	 * returns the byte[] that represents E( session request || username ||
+	 * HMAC(MSG, HK), SU ) that should be sent from the phone to the server
+	 * every time a new session key is needed. As parameters, it takes the
+	 * Symmetric Secret key, the current session key (null if the first time),
+	 * the username for the client, and the server's public key to encrypt the
+	 * whole thing.
 	 */
-	public static byte[] sessionRequest(Key psk, Key sk, String user, PublicKey su) throws InvalidKeyException, UnsupportedEncodingException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+	public static byte[] sessionRequest(Key psk, Key sk, String user,
+			PublicKey su) throws InvalidKeyException,
+			UnsupportedEncodingException, NoSuchAlgorithmException,
+			NoSuchPaddingException, IllegalBlockSizeException,
+			BadPaddingException {
 		String keyString;
 		if (sk == null)
 			keyString = psk.toString();
-		else{
+		else {
 			byte[] k1 = psk.getEncoded();
 			byte[] k2 = sk.getEncoded();
 			byte[] xor = new byte[k1.length];
-			for (int i =0; i<k1.length; i++){
+			for (int i = 0; i < k1.length; i++) {
 				xor[i] = (byte) (k1[i] ^ k2[i]);
 			}
 			keyString = new String(xor);
 		}
-		String payload = SESSION_REQUEST+user;
+		String payload = SESSION_REQUEST + user;
 		payload += hmacDigest(payload, keyString);
-		
+
 		Cipher asymCipher = getAsymmetricCipher();
 		asymCipher.init(Cipher.ENCRYPT_MODE, su);
 		return asymCipher.doFinal(payload.getBytes());
 	}
-	
+
 	/*
-	 * returns a new IV, to be used with symmetric key encription since we are using CBC
+	 * returns a new IV, to be used with symmetric key encription since we are
+	 * using CBC
 	 */
-	public static IvParameterSpec getNewIV(){
+	public static IvParameterSpec getNewIV() {
 		SecureRandom random = new SecureRandom();
 		byte[] ivBytes = new byte[CryptoUtilities.IV_LENGTH];
 		random.nextBytes(ivBytes);
 		return new IvParameterSpec(ivBytes);
 	}
-	
+
 	/*
-	 * convenience method to return a symmetric cipher using the algorithms and modes we've 
-	 * specified in our documentation.
+	 * convenience method to return a symmetric cipher using the algorithms and
+	 * modes we've specified in our documentation.
 	 */
-	public static Cipher getSymmetricCipher() throws NoSuchAlgorithmException, NoSuchPaddingException{
-		return Cipher.getInstance(SYMMETRIC_KEY_ALGORITHM + "/" + SYM_CYPHER_MODE + "/" + SYM_PADDING_TYPE);
+	public static Cipher getSymmetricCipher() throws NoSuchAlgorithmException,
+			NoSuchPaddingException {
+		return Cipher.getInstance(SYMMETRIC_KEY_ALGORITHM + "/"
+				+ SYM_CYPHER_MODE + "/" + SYM_PADDING_TYPE);
 	}
-	
+
 	/*
-	 * convenience method to return an asymmetric cipher using the algorithms and modes we've 
-	 * specified in our documentation. 
+	 * convenience method to return an asymmetric cipher using the algorithms
+	 * and modes we've specified in our documentation.
 	 */
-	public static Cipher getAsymmetricCipher() throws NoSuchAlgorithmException, NoSuchPaddingException{
-		return Cipher.getInstance(ASYMMETRIC_KEY_ALGORITHM + "/" + ASYM_CYPHER_MODE + "/" + ASYM_PADDING_TYPE);
+	public static Cipher getAsymmetricCipher() throws NoSuchAlgorithmException,
+			NoSuchPaddingException {
+		return Cipher.getInstance(ASYMMETRIC_KEY_ALGORITHM + "/"
+				+ ASYM_CYPHER_MODE + "/" + ASYM_PADDING_TYPE);
 	}
-	
+
 	/*
-	 * Convenience function used to allow the utilization of large keys for encryption. You will need to call
-	 * this at the beginning of any "main" function. If you fail to, you will get:
+	 * Convenience function used to allow the utilization of large keys for
+	 * encryption. You will need to call this at the beginning of any "main"
+	 * function. If you fail to, you will get:
 	 * java.security.InvalidKeyException: Invalid Key Length
-	 * 
 	 */
-	public static void allowEncryption(){
+	public static void allowEncryption() {
 		try {
-	        java.lang.reflect.Field field = Class.forName("javax.crypto.JceSecurity").getDeclaredField("isRestricted");
-	        field.setAccessible(true);
-	        field.set(null, java.lang.Boolean.FALSE);
-	    } catch (Exception ex) {
-	        ex.printStackTrace();
-	    }
+			java.lang.reflect.Field field = Class.forName(
+					"javax.crypto.JceSecurity")
+					.getDeclaredField("isRestricted");
+			field.setAccessible(true);
+			field.set(null, java.lang.Boolean.FALSE);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
-	
 
 	/*
 	 * convenience method to get symmetric key
 	 */
-	public static Key getSymmetricKey() throws NoSuchAlgorithmException{
+	public static Key getSymmetricKey() throws NoSuchAlgorithmException {
 		KeyGenerator kg = KeyGenerator.getInstance(SYMMETRIC_KEY_ALGORITHM);
 		kg.init(SYM_KEY_SIZE);
 		return kg.generateKey();
 	}
-	
+
 	/*
-	 * convenience method to get asymmetric key. Use bigKey=true for the server key
+	 * convenience method to get asymmetric key. Use bigKey=true for the server
+	 * key
 	 */
-	public static KeyPair getKeypair(boolean bigKey) throws NoSuchAlgorithmException{
-		KeyPairGenerator keyGen = KeyPairGenerator.getInstance(CryptoUtilities.ASYMMETRIC_KEY_ALGORITHM);
-	    if (bigKey)
-	    	keyGen.initialize(CryptoUtilities.ASYM_KEY_SIZE*2);
-	    else
-	    	keyGen.initialize(CryptoUtilities.ASYM_KEY_SIZE);
-	    return keyGen.generateKeyPair();
+	public static KeyPair getKeypair(boolean bigKey)
+			throws NoSuchAlgorithmException {
+		KeyPairGenerator keyGen = KeyPairGenerator
+				.getInstance(CryptoUtilities.ASYMMETRIC_KEY_ALGORITHM);
+		if (bigKey)
+			keyGen.initialize(CryptoUtilities.ASYM_KEY_SIZE * 2);
+		else
+			keyGen.initialize(CryptoUtilities.ASYM_KEY_SIZE);
+		return keyGen.generateKeyPair();
 	}
 
 	/*
 	 * returns a byte[] that is E( E( SK0 || TL, PU) , SR)
 	 */
-	public static byte[] getNewSessionResponse(PublicKey pu, PrivateKey sr, int tl) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		
+	public static byte[] getNewSessionResponse(PublicKey pu, PrivateKey sr,
+			int tl) throws NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+
 		Key newSessionKey = getSymmetricKey();
 		byte[] keyBytes = newSessionKey.getEncoded();
 		byte[] timesLeftBytes = ByteBuffer.allocate(4).putInt(tl).array();
-		byte[] payload = new byte[keyBytes.length+timesLeftBytes.length];
+		byte[] payload = new byte[keyBytes.length + timesLeftBytes.length];
 		System.arraycopy(keyBytes, 0, payload, 0, keyBytes.length);
-		System.arraycopy(timesLeftBytes,0,payload,keyBytes.length,timesLeftBytes.length);
-		
+		System.arraycopy(timesLeftBytes, 0, payload, keyBytes.length,
+				timesLeftBytes.length);
+
 		Cipher asymCipher = getAsymmetricCipher();
 		asymCipher.init(Cipher.ENCRYPT_MODE, pu);
 		byte[] halfway = asymCipher.doFinal(payload);
-		
+
 		asymCipher.init(Cipher.ENCRYPT_MODE, sr);
 		return asymCipher.doFinal(halfway);
-		
+
 	}
 
 	/*
 	 * Adding a method for convenient HMAC generation
 	 */
-	 public static String hmacDigest(String msg, String keyString) throws UnsupportedEncodingException, NoSuchAlgorithmException, InvalidKeyException {
-		 String digest = null;
-		 SecretKeySpec key = new SecretKeySpec((keyString).getBytes(PLAINTEXT_ENCODING), SYMMETRIC_KEY_ALGORITHM);
-		 Mac mac = Mac.getInstance(HMAC_ALGORITHM);
-		 mac.init(key);
+	public static String hmacDigest(String msg, String keyString)
+			throws UnsupportedEncodingException, NoSuchAlgorithmException,
+			InvalidKeyException {
+		String digest = null;
+		SecretKeySpec key = new SecretKeySpec(
+				(keyString).getBytes(PLAINTEXT_ENCODING),
+				SYMMETRIC_KEY_ALGORITHM);
+		Mac mac = Mac.getInstance(HMAC_ALGORITHM);
+		mac.init(key);
 
-		 byte[] bytes = mac.doFinal(msg.getBytes(PLAINTEXT_ENCODING));//May not need to convert it to UTF8 - depends what the server will use
+		byte[] bytes = mac.doFinal(msg.getBytes(PLAINTEXT_ENCODING));
+		StringBuffer hash = new StringBuffer();
+		for (int i = 0; i < bytes.length; i++) {
+			String hex = Integer.toHexString(0xFF & bytes[i]);
+			if (hex.length() == 1) {
+				hash.append('0');
+			}
+			hash.append(hex);
+		}
+		digest = hash.toString();
 
-		 StringBuffer hash = new StringBuffer();
-		 for (int i = 0; i < bytes.length; i++) {
-			 String hex = Integer.toHexString(0xFF & bytes[i]);
-			 if (hex.length() == 1) {
-				 hash.append('0');
-			 }
-			 hash.append(hex);
-		 }
-		 digest = hash.toString();
+		return digest;
+	}
 
-		 return digest;
-	 }
-	
 }
