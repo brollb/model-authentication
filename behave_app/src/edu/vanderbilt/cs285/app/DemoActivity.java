b053d9a39169b07e313d808dc89779d360b1d049
Ben Draffin <ben.draffin@sv.cmu.edu>
2013-12-02 12:34:11 -0600

    Updated Android Application for networking -- Only runs on emulator

--- behave_app/src/edu/vanderbilt/cs285/app/DemoActivity.java
+++ behave_app/src/edu/vanderbilt/cs285/app/DemoActivity.java
@@ -12,7 +12,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class DemoActivity extends Activity {
-	public static final String serverURLString = "10.0.2.2"; // Emulator Uses 10.0.2.2 for localhost
+	// the networking will ONLY function if this is running on an emulator
+	public static final String serverURLString = "http://10.0.2.2:8000/test"; // Emulator Uses 10.0.2.2 for localhost
 	public int checksLeft = 10; 
 	TextView console = null;
 	TextView timesleft= null;
@@ -21,9 +22,9 @@ public class DemoActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//console  = getResources()  // Find view from id text_console
-		//timesleft = getResources()  // Find view from id text_model_checks_left
-		//setContentView(.layout.activity_demo);
+		setContentView(R.layout.activity_demo);
+		console = (TextView)findViewById(R.id.text_console);
+		timesleft = (TextView)findViewById(R.id.text_model_checks_left);
 		
 		try {
 			serverURL = new URL(serverURLString);
@@ -31,12 +32,14 @@ public class DemoActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		timesleft.setText(checksLeft + "");
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(.menu.demo, menu);
+		getMenuInflater().inflate(R.menu.demo, menu);
 		return true;
 	}
 	
@@ -46,10 +49,26 @@ public class DemoActivity extends Activity {
 				Toast.LENGTH_SHORT).show();
 	}
 	
-	public void writeToConsole(String msg) {
-		console.setText(console.getText() + "\n" + msg);
+	public void writeToConsole(final String msg) {
+	    DemoActivity.this.runOnUiThread(new Runnable() {
+	    	public void run() {
+	    		try {
+	    			console.setText(msg + "\n" + console.getText());
+	    		} catch(Exception e){}
+	    	}
+	    });
 	}
 	
+	public void decrementChecksLeft() {
+		checksLeft--;
+		DemoActivity.this.runOnUiThread(new Runnable() {
+	    	public void run() {
+	    		try {
+	    			timesleft.setText(checksLeft + "");
+	    		} catch(Exception e){}
+	    	}
+	    });
+	}
 
 	public void btn_init_onclick(View view)  
 	{  
@@ -57,21 +76,55 @@ public class DemoActivity extends Activity {
 	    
 	}  
 	
+//	public void postData() {
+//	    // Create a new HttpClient and Post Header
+//	    HttpClient httpclient = new DefaultHttpClient();
+//	    HttpPost httppost = new HttpPost(serverURLString);
+//
+//	    try {
+//	        // Add your data
+//	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+//	        nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+//	        nameValuePairs.add(new BasicNameValuePair("stringdata", "Hi"));
+//	        httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//
+//	        // Execute HTTP Post Request
+//	        HttpResponse response = httpclient.execute(httppost);
+//
+//	    } catch (ClientProtocolException e) {
+//	        // TODO Auto-generated catch block
+//	    } catch (IOException e) {
+//	        // TODO Auto-generated catch block
+//	    }
+//	} 
+	
 	public void btn_unencrypt_onclick(View view)  
 	{  
-	    Toast.makeText(this, "Test Unecrypt Button clicked!", Toast.LENGTH_SHORT).show();
+	    final DemoActivity da = this;
+		Toast.makeText(this, "Test Unecrypt Button clicked!", Toast.LENGTH_SHORT).show();
 	    
+		// It will only connect while running on an emulator
 	    if(false == Network.canConnectToServer(serverURLString))
 	    	cantConnect();
 	    
-	    HttpPostRequest req = new HttpPostRequest();
-	    Hashtable<String, String> params = new Hashtable<String, String>();
-	    params.put("userID", "bdraff");
-	    params.put("checksleft", checksLeft+"");
-	    params.put("timestamp", System.currentTimeMillis()+"");
-	    writeToConsole("Sending to " + serverURLString + ": " + params.toString() + "... ");
-	    String response = req.send(serverURLString, params);
-	    writeToConsole("Response Received. msg: " + response);
+	    new Thread() {
+	    	public void run() {
+	    
+		    HttpPostRequest req = new HttpPostRequest();
+		    Hashtable<String, String> params = new Hashtable<String, String>();
+		    
+		    // POST Parameters
+		    params.put("userID", "bdraff");
+		    params.put("checksleft", checksLeft+"");
+		    params.put("timestamp", System.currentTimeMillis()+"");
+		    writeToConsole("Sending to " + serverURLString + ": " + params.toString() + "... ");
+		    final String response = req.send(serverURLString, params);
+		    System.out.println("Server Response: " + response);
+		    writeToConsole("Response Received. msg: " + response);
+		    decrementChecksLeft();
+	    	}
+	    }.start();
+	    
 	    
 	    
 	}  
